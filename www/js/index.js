64189b2cc0b264b2fe32bd4b48b33e51c9224d77
vipzz <46714992+vipzz@users.noreply.github.com>
2019-01-15 19:15:03 +0530

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -58,7 +58,7 @@ function onOffline() {
 }
 
 function openBrowser() {
-   var url = 'https://dusthqdenevents.frb.io/';
+   var url = 'http://infinova.enceplon.com/';
    var target = '_blank';
    var options = "location=no,toolbar=no"
    var ref = cordova.InAppBrowser.open(url, target, options);
